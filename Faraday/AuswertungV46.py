a8e4112731cb390b6e9f4e5c1e405eece128db24
xxholzixx1 <michel30072002@gmail.com>
2022-04-25 16:19:03 +0200

    noch ntest

---
+++ Faraday/AuswertungV46.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import uncertainties as unc
+import uncertainties.unumpy as unp
+from uncertainties import ufloat
+from scipy.optimize import curve_fit
+import scipy.constants as const
+import sympy
+import os
